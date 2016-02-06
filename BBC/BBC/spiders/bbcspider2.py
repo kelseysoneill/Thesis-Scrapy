80fc0e51421b032c502a728ef87937ac0dd36fe8
kelseysoneill <kelseyoneill2015@u.northwestern.edu>
2016-02-05 18:17:37 -0800

    Regex changes for BBC

---
+++ BBC/BBC/spiders/bbcspider2.py
@@ -0,0 +1,38 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from BBC.items import BbcItem  # item class 
+from scrapy.selector import Selector
+import re
+
+
+
+class MySpider(CrawlSpider):
+    name = "bbc2"  # unique identifier for the spider
+    allowed_domains = ['bbc.com']  # limits the crawl to this domain list
+    start_urls = [
+    'http://www.bbc.com/news/election/us2016'
+    ]
+    rules = [
+        Rule(LinkExtractor(allow=r'world-us'),
+         callback='parse_item', follow=True)
+    ]
+
+    def parse_item(self, response):
+        sel = Selector(response)
+        results = []
+        item = BbcItem()
+        item['article_title'] = sel.xpath('//h1[@class="story-body__h1"]/text()').extract()
+        item['article_imagecaption'] = sel.xpath('//figure[@class="media-landscape has-caption full-width"]/figcaption/span[2]/text()').extract()
+        item['article_author'] = sel.xpath('//div[@class="byline__name-and-title"]/a/span/text()').extract()
+        #item['article_preview'] = sel.xpath('//p[@class="story-body__introduction"]/text()').extract()
+        #item['article_highlights'] = sel.xpath('.//div[@class="el__storyhighlights"]/ul/li/text()').extract()
+        #item['article_edsource'] = sel.xpath('//*[@id="article"]/div[2]/div/div[1]/div[2]/p[1]/text()').extract()
+        temparticlecontent = sel.xpath('//div[@class="story-body__inner"]//text()').extract()  
+        temparticlecontent = re.sub(r'(\n)|(\s{2,})', '', temparticlecontent)
+        #item['article_content'] = temparticlecontent.re(r'<[^>]*?>', ' ', str(site[0].extract()))
+
+        item['article_timestamp'] = sel.xpath('//div[@class="story-body__mini-info-list-and-share"]/ul/li/div/text()').extract()
+        item['article_url'] = sel.xpath('//*[@id="responsive-news"]/head/meta[15]/@content').extract()
+        results.append(item)
+        return results
